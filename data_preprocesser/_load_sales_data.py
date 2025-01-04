35fa93ede446cf40a25637a36885f39fe1c1d66f
stilver10 <stilver10@gmail.com>
2025-01-04 20:37:04 +0900

    수량, 단가, 금액의 공백 처리 및 무결성 확인 기능 추가

--- data_preprocesser/_load_sales_data.py
+++ data_preprocesser/_load_sales_data.py
@@ -15,13 +15,20 @@ def load_sales_data(file_path: Path) -> pd.DataFrame:
         # 형식 변환
         raw_data['매출년도'] = raw_data['매출년도'].astype(str).str.extract(r'(\d{4})').astype(int)
         raw_data['매출월'] = raw_data['매출월'].astype(str).str.extract(r'(\d+)').astype(int)
-        raw_data['수량'] = raw_data['수량'].fillna(0).astype(float)
-        raw_data['금액'] = raw_data['금액'].fillna(0).astype(float)
+        raw_data['수량'] = raw_data['수량'].replace(r'^\s*$', '0', regex=True).astype(float)
+        raw_data['단가'] = raw_data['단가'].replace(r'^\s*$', '0', regex=True).astype(float)
+        raw_data['금액'] = raw_data['금액'].replace(r'^\s*$', '0', regex=True).astype(float)
 
-        # # 무한대 값 제거 및 단가가 0인 경우 금액을 0으로 설정
+
+        # 무한대 값 제거 및 단가가 0인 경우 금액을 0으로 설정
         # raw_data.replace([np.inf, -np.inf], np.nan, inplace=True)
         # raw_data.loc[raw_data['단가'] == 0, '금액'] = 0
-        ############## 값의 무결성 확인 (수량*단가 == 금액) ##############
+
+
+        #  값의 무결성 확인 (수량*단가 == 금액) 
+        a = raw_data.apply(lambda row: row['수량'] * row['단가'], axis=1)
+        raw_data['무결성'] = raw_data['금액'] == a
+
         null_counts = raw_data.isnull().sum()
         print("### Null 값 개수: ###\n", null_counts)
         print("### 각 행의 Null 값: ###\n", raw_data[raw_data.isna().any(axis=1)])
--- main.py
+++ main.py
@@ -9,18 +9,19 @@ def main():
     print(f'저장 파일 위치: {current_directory}')
 
     # 데이터베이스 파일 경로 설정
-    db_file_path = Path('./2018~2022 매출 데이터베이스.xlsx')
+    db_file_path = Path('c:/Users/hyeon/Desktop/영업부 코드/2018~2022 매출 데이터베이스.xlsx')
     print(f'db 파일 경로: {db_file_path}')
 
     # 원본 데이터 로드
     raw_data = load_sales_data(db_file_path)
     valid_years = calculate_years(raw_data, start_year = None, end_year = None)
-    print('##############', valid_years, '##############')
+    print('##############','\t', '조회년도:', valid_years, '\t', '##############')
 
     monthly_report = monthly_sales_and_revenue(raw_data, valid_years)
     customer_report = sales_order_customer(raw_data, valid_years)
     product_report = sales_order_product(raw_data, valid_years)
     print(monthly_report, '\n', customer_report, '\n', product_report, '\n', )
+
 if __name__ == "__main__":
     # Execute when the module is not initialized from an import statement.
     main()
