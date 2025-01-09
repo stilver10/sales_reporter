ce4b8a227e9ae4c87591edaac6e25d04f8cb7792
stilver10 <stilver10@gmail.com>
2025-01-10 06:30:19 +0900

    차트 모듈 경로 수정: 연간 및 고객 차트 임포트 경로 변경

--- plot/__init__.py
+++ plot/__init__.py
@@ -1,5 +1,5 @@
-from .seaborn_chart import yearly_report_chart
-from .pie_chart import customer_report_chart, customer_historical_chart
+from .yearly_chart import yearly_report_chart
+from .customer_chart import customer_report_chart, customer_historical_chart
 from .test import test_chart
 
 __all__ = ['yearly_report_chart', 'customer_report_chart', 'customer_historical_chart']
